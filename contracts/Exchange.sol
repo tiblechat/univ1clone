aea1d32d7984e8107d80c305a04653369c7d6de2
tib <thibof@hotmail.com>
2022-11-03 15:32:19 +0100

    Exchange finish implem

--- contracts/Exchange.sol
+++ contracts/Exchange.sol
@@ -1,23 +1,86 @@
 // contracts/Exchange.sol
 pragma solidity ^0.8.9;
 
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
-
-contract Exchange 
+contract Exchange is ERC20 
 {
     address tokenAddress;
-    constructor(address _token) 
+    constructor(address _token) ERC20("Zuniswap-V1", "ZUNI-V1") 
     {
         require(_token != address(0), "invalid token address");
         tokenAddress = _token;
     }
 
-    function addLiquidity(uint256 _tokenAmount) external payable
+
+    function getReserve() public view returns (uint256)
+    {
+        return IERC20(tokenAddress).balanceOf(address(this));
+    }
+
+    function getAmount(uint256 inputAmount,uint256 inputReserve,uint256 outputReserve) private pure returns (uint256) 
+    {
+        require(inputReserve > 0 && outputReserve > 0, "invalid reserves");
+
+
+        // we take 1% because why not
+        uint256 inputAmountWithFee = inputAmount * 99;
+
+        uint256 numerator = inputAmountWithFee * outputReserve;
+        uint256 denominator = (inputReserve * 100) + inputAmountWithFee;
+
+        return numerator / denominator;
+    }
+
+
+
+
+
+    function getTokenAmount(uint256 _ethSold) public view returns (uint256) {
+      require(_ethSold > 0, "ethSold is too small");
+      uint256 tokenReserve = getReserve();
+      return getAmount(_ethSold, address(this).balance, tokenReserve);
+    }
+    
+    function getEthAmount(uint256 _tokenSold) public view returns (uint256) 
+    {
+      require(_tokenSold > 0, "tokenSold is too small");
+      uint256 tokenReserve = getReserve();
+      return getAmount(_tokenSold, tokenReserve, address(this).balance);
+    }
+
+    function ethToTokenSwap(uint256 _minTokens) public payable 
+    {
+      uint256 tokenReserve = getReserve();
+      uint256 tokensBought = getAmount(msg.value,address(this).balance - msg.value,tokenReserve);
+      require(tokensBought >= _minTokens, "insufficient output amount");
+      IERC20(tokenAddress).transfer(msg.sender, tokensBought);
+    }
+    
+    function tokenToEthSwap(uint256 _tokensSold, uint256 _minEth) public 
+    {
+      uint256 tokenReserve = getReserve();
+      uint256 ethBought = getAmount(_tokensSold,tokenReserve,address(this).balance);
+
+      require(ethBought >= _minEth, "insufficient output amount");
+
+      IERC20(tokenAddress).transferFrom(msg.sender, address(this), _tokensSold);
+      payable(msg.sender).transfer(ethBought);
+    }
+
+
+    function addLiquidity(uint256 _tokenAmount) external payable returns (uint256 liquidity)
     {
         if (getReserve() == 0) {
             IERC20 token = IERC20(tokenAddress);
             token.transferFrom(msg.sender, address(this), _tokenAmount);
+
+            uint256 liquidity = address(this).balance;
+            _mint(msg.sender, liquidity);
+            return liquidity;
+
+
         }
         else
         {
@@ -28,57 +91,26 @@ contract Exchange
 
             IERC20 token = IERC20(tokenAddress);
             token.transferFrom(msg.sender, address(this), tokenAmount);
+
+            liquidity = (msg.value * totalSupply()) / ethReserve;
+            _mint(msg.sender, liquidity);
         }
     }
 
-    function getReserve() public view returns (uint256)
-     {
-        return IERC20(tokenAddress).balanceOf(address(this));
-    }
 
-    function getAmount(uint256 inputAmount,uint256 inputReserve,uint256 outputReserve) private pure returns (uint256) 
+    function removeLiquidity(uint256 _amount) external returns (uint256, uint256) 
     {
-        require(inputReserve > 0 && outputReserve > 0, "invalid reserves");
-        return (inputAmount * outputReserve) / (inputReserve + inputAmount);
+      require(_amount > 0, "invalid amount");
+
+      uint256 ethAmount = (address(this).balance * _amount) / totalSupply();
+      uint256 tokenAmount = getReserve() * _amount/ totalSupply();
+
+      _burn(msg.sender,_amount);
+      payable(msg.sender).transfer(ethAmount);
+      IERC20(tokenAddress).transfer(msg.sender,tokenAmount);
+
+      return (ethAmount,tokenAmount);
+
     }
 
-function getTokenAmount(uint256 _ethSold) public view returns (uint256) {
-  require(_ethSold > 0, "ethSold is too small");
-
-  uint256 tokenReserve = getReserve();
-
-  return getAmount(_ethSold, address(this).balance, tokenReserve);
-}
-
-function getEthAmount(uint256 _tokenSold) public view returns (uint256) {
-  require(_tokenSold > 0, "tokenSold is too small");
-
-  uint256 tokenReserve = getReserve();
-
-  return getAmount(_tokenSold, tokenReserve, address(this).balance);
-}
-
-function ethToTokenSwap(uint256 _minTokens) public payable {
-    uint256 tokenReserve = getReserve();
-    uint256 tokensBought = getAmount(
-      msg.value,
-      address(this).balance - msg.value,
-      tokenReserve
-    );
-  
-    require(tokensBought >= _minTokens, "insufficient output amount");
-  
-    IERC20(tokenAddress).transfer(msg.sender, tokensBought);
-  }
-
-  function tokenToEthSwap(uint256 _tokensSold, uint256 _minEth) public 
-  {
-    uint256 tokenReserve = getReserve();
-    uint256 ethBought = getAmount(_tokensSold,tokenReserve,address(this).balance);
-
-      require(ethBought >= _minEth, "insufficient output amount");
-
-      IERC20(tokenAddress).transferFrom(msg.sender, address(this), _tokensSold);
-      payable(msg.sender).transfer(ethBought);
-    }
 }
